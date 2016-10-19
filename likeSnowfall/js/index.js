9440ebbd65b9a5f815cf48f61c7d24168dacfc20
schuylerwilliams <schuylerwilliams@Sky-Pro.local>
2016-10-19 11:22:55 -0500

    update

--- likeSnowfall/js/index.js
+++ likeSnowfall/js/index.js
@@ -57,42 +57,43 @@ $(window).scroll(function(){
 	*/
 		
 	
-	if (scroll > 50 && scroll <= 100) {
+	if (scroll > 200 && scroll <= 225) {
 		document.getElementById('testScroll').style.backgroundColor = "rgba(255,255,255,0.25)";
-	}
-	else if (scroll > 100 && scroll <= 150) {
-		document.getElementById('testScroll').style.backgroundColor = "rgba(255,255,255,0.3)";
-	} else if (scroll > 150 && scroll <= 200) {
-		document.getElementById('testScroll').style.backgroundColor = "rgba(255,255,255,0.35)";
-	} else if (scroll > 200 && scroll <= 250) {
-		document.getElementById('testScroll').style.backgroundColor = "rgba(255,255,255,0.40)";
 		document.getElementById('headerMenus').style.visibility = "visible";
-	} else if (scroll > 250 && scroll <= 300) {
-		document.getElementById('testScroll').style.backgroundColor = "rgba(255,255,255,0.45)";
+	}
+	else if (scroll > 225 && scroll <= 250) {
+		document.getElementById('testScroll').style.backgroundColor = "rgba(255,255,255,0.3)";
+	} else if (scroll > 250 && scroll <= 275) {
+		document.getElementById('testScroll').style.backgroundColor = "rgba(255,255,255,0.35)";
+	} else if (scroll > 275 && scroll <= 300) {
+		document.getElementById('testScroll').style.backgroundColor = "rgba(255,255,255,0.40)";
+		//document.getElementById('headerMenus').style.visibility = "visible";
 	} else if (scroll > 300 && scroll <= 350) {
+		document.getElementById('testScroll').style.backgroundColor = "rgba(255,255,255,0.45)";
+	} else if (scroll > 350 && scroll <= 375) {
 		document.getElementById('testScroll').style.backgroundColor = "rgba(255,255,255,0.50)";
 		//document.getElementById('headerMenus').style.visibility = "visible";
-	} else if (scroll > 350 && scroll <= 400) {
+	} else if (scroll > 375 && scroll <= 400) {
 		document.getElementById('testScroll').style.backgroundColor = "rgba(255,255,255,0.55)";
-	} else if (scroll > 400 && scroll <= 450) {
+	} else if (scroll > 400 && scroll <= 425) {
 		document.getElementById('testScroll').style.backgroundColor = "rgba(255,255,255,0.60)";
-	} else if (scroll > 450 && scroll <= 500) {
+	} else if (scroll > 425 && scroll <= 450) {
 		document.getElementById('testScroll').style.backgroundColor = "rgba(255,255,255,0.65)";
-	} else if (scroll > 550 && scroll <= 600) {
+	} else if (scroll > 450 && scroll <= 475) {
 		document.getElementById('testScroll').style.backgroundColor = "rgba(255,255,255,0.70)";
-	} else if (scroll > 600 && scroll <= 650) {
+	} else if (scroll > 475 && scroll <= 500) {
 		document.getElementById('testScroll').style.backgroundColor = "rgba(255,255,255,0.75)";
-	} else if (scroll > 650 && scroll <= 700) {
+	} else if (scroll > 500 && scroll <= 525) {
 		document.getElementById('testScroll').style.backgroundColor = "rgba(255,255,255,0.80)";
-	} else if (scroll > 700 && scroll <= 750) {
+	} else if (scroll > 525 && scroll <= 550) {
 		document.getElementById('testScroll').style.backgroundColor = "rgba(255,255,255,0.85)";
-	} else if (scroll > 750 && scroll <= 800) {
+	} else if (scroll > 550 && scroll <= 575) {
 		document.getElementById('testScroll').style.backgroundColor = "rgba(255,255,255,0.90)";
-	} else if (scroll > 800 && scroll <= 850) {
+	} else if (scroll > 575 && scroll <= 600) {
 		document.getElementById('testScroll').style.backgroundColor = "rgba(255,255,255,0.95)";
-	} else if (scroll > 850 && scroll <= 900) {
+	} else if (scroll > 600 && scroll <= 625) {
 		document.getElementById('testScroll').style.backgroundColor = "rgba(255,255,255,1)";
-	} else if (scroll > 0 && scroll <= 50) {
+	} else if (scroll > 0 && scroll <= 200) {
 		document.getElementById('testScroll').style.backgroundColor = "rgba(0,0,0,0.35)";
 	}
 	
