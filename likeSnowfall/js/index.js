5a7389fded06d81a8e139e032988abb319807cd3
schuylerwilliams <schuylerwilliams@Sky-Pro.local>
2016-10-19 10:35:14 -0500

    update

--- likeSnowfall/js/index.js
+++ likeSnowfall/js/index.js
@@ -50,6 +50,8 @@ $(window).scroll(function(){
 		document.getElementById('testScroll').style.backgroundColor = "rgba(255,255,255,0.5)"
 	} else if (scroll > 400 && scroll <= 600) {
 		document.getElementById('testScroll').style.backgroundColor = "rgba(255,255,255,0.7)"
+	} else if (scroll > 0 && scroll < 150) {
+		document.getElementById('testScroll').style.backgroundColor = "rgba(0,0,0,0.5)"
 	}
 	/*
 	var a = $('header').height() * .7;
