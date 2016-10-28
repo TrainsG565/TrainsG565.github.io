1876398405ffd75c562b7541121d4582e8472231
schuylerwilliams <schuylerwilliams@Sky-Pro.local>
2016-10-28 11:02:09 -0500

    update

--- likeSnowfall/js/initMap.js
+++ likeSnowfall/js/initMap.js
@@ -5,10 +5,43 @@ mapboxgl.accessToken = 'pk.eyJ1Ijoic2t5d2lsbGlhbXMiLCJhIjoibUI4TlByNCJ9.9UuhBU3E
 var map01 = new mapboxgl.Map({
     container: 'map01',
     style: 'mapbox://styles/mapbox/light-v8',
-    center: [-89.7372, 43.4316],
+    center: [-89.4012, 43.0731],
     zoom: 12,
     attributionControl: false
 });
+
+map01.on('style.load', function () {
+	map01.addSource('tax-parcels', {
+		'type': 'geojson',
+		'data': 'data/tax_parcels.geojson'
+	});
+	
+	map01.addLayer({
+		'id': 'tax-parcels',
+		'type': 'fill',
+		'source': 'tax-parcels',
+		'paint': {
+			'fill-color': 'orange',
+			'fill-extrude-height': 50,
+			'fill-extrude-base': 0,
+			'fill-opacity': 0.5
+		}
+	});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 var map02 = new mapboxgl.Map({
 	container: 'map02',
