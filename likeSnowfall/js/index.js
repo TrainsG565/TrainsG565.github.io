5ce131bad1ddd500931a08d5edacacae00dc9b8b
schuylerwilliams <schuylerwilliams@Sky-Pro.local>
2016-11-01 00:06:49 -0500

    lotsa updates

--- likeSnowfall/js/index.js
+++ likeSnowfall/js/index.js
@@ -58,43 +58,49 @@ $(window).scroll(function(){
 		
 	
 	if (scroll > 200 && scroll <= 225) {
-		document.getElementById('testScroll').style.backgroundColor = "rgba(0,0,0,0.15)";
+		document.getElementById('testScroll').style.backgroundColor = "rgba(255,255,255,0.4)";
 		document.getElementById('headerMenus').style.visibility = "visible";
-	} 
-	else if (scroll > 225 && scroll <= 250) {
-		document.getElementById('testScroll').style.backgroundColor = "rgba(255,255,255,0.15)";
-	} else if (scroll > 250 && scroll <= 275) {
+	} else if (scroll > 50 && scroll <= 75) {
+		document.getElementById('testScroll').style.backgroundColor = "rgba(255,255,255,0.3)";
+	} else if (scroll > 75 && scroll <= 150) {
 		document.getElementById('testScroll').style.backgroundColor = "rgba(255,255,255,0.35)";
+	} else if (scroll > 150 && scroll <= 200) {
+		document.getElementById('testScroll').style.backgroundColor = "rgba(255,255,255,0.37)";
+	}
+	else if (scroll > 225 && scroll <= 250) {
+		document.getElementById('testScroll').style.backgroundColor = "rgba(255,255,255,0.42)";
+	} else if (scroll > 250 && scroll <= 275) {
+		document.getElementById('testScroll').style.backgroundColor = "rgba(255,255,255,0.45)";
 	} else if (scroll > 275 && scroll <= 300) {
-		document.getElementById('testScroll').style.backgroundColor = "rgba(255,255,255,0.40)";
+		document.getElementById('testScroll').style.backgroundColor = "rgba(255,255,255,0.47)";
 		//document.getElementById('headerMenus').style.visibility = "visible";
 	} else if (scroll > 300 && scroll <= 350) {
-		document.getElementById('testScroll').style.backgroundColor = "rgba(255,255,255,0.45)";
+		document.getElementById('testScroll').style.backgroundColor = "rgba(255,255,255,0.5)";
 	} else if (scroll > 350 && scroll <= 375) {
-		document.getElementById('testScroll').style.backgroundColor = "rgba(255,255,255,0.50)";
+		document.getElementById('testScroll').style.backgroundColor = "rgba(255,255,255,0.55)";
 		//document.getElementById('headerMenus').style.visibility = "visible";
 	} else if (scroll > 375 && scroll <= 400) {
-		document.getElementById('testScroll').style.backgroundColor = "rgba(255,255,255,0.55)";
+		document.getElementById('testScroll').style.backgroundColor = "rgba(255,255,255,0.6)";
 	} else if (scroll > 400 && scroll <= 425) {
-		document.getElementById('testScroll').style.backgroundColor = "rgba(255,255,255,0.60)";
-	} else if (scroll > 425 && scroll <= 450) {
 		document.getElementById('testScroll').style.backgroundColor = "rgba(255,255,255,0.65)";
+	} else if (scroll > 425 && scroll <= 450) {
+		document.getElementById('testScroll').style.backgroundColor = "rgba(255,255,255,0.7)";
 	} else if (scroll > 450 && scroll <= 475) {
-		document.getElementById('testScroll').style.backgroundColor = "rgba(255,255,255,0.70)";
-	} else if (scroll > 475 && scroll <= 500) {
 		document.getElementById('testScroll').style.backgroundColor = "rgba(255,255,255,0.75)";
+	} else if (scroll > 475 && scroll <= 500) {
+		document.getElementById('testScroll').style.backgroundColor = "rgba(255,255,255,0.8)";
 	} else if (scroll > 500 && scroll <= 525) {
-		document.getElementById('testScroll').style.backgroundColor = "rgba(255,255,255,0.80)";
-	} else if (scroll > 525 && scroll <= 550) {
 		document.getElementById('testScroll').style.backgroundColor = "rgba(255,255,255,0.85)";
+	} else if (scroll > 525 && scroll <= 550) {
+		document.getElementById('testScroll').style.backgroundColor = "rgba(255,255,255,0.9)";
 	} else if (scroll > 550 && scroll <= 575) {
-		document.getElementById('testScroll').style.backgroundColor = "rgba(255,255,255,0.90)";
-	} else if (scroll > 575 && scroll <= 600) {
 		document.getElementById('testScroll').style.backgroundColor = "rgba(255,255,255,0.95)";
+	} else if (scroll > 575 && scroll <= 600) {
+		document.getElementById('testScroll').style.backgroundColor = "rgba(255,255,255,1)";
 	} else if (scroll > 600 && scroll <= 625) {
 		document.getElementById('testScroll').style.backgroundColor = "rgba(255,255,255,1)";
-	} else if (scroll > 0 && scroll <= 200) {
-		document.getElementById('testScroll').style.backgroundColor = "rgba(0,0,0,0.35)";
+	} else if (scroll > 0 && scroll <= 50) {
+		document.getElementById('testScroll').style.backgroundColor = "rgba(255,255,255,0.25)";
 	}
 	
 	
