03c368093c4ca5d6ce5f8c380131072739cb5b5b
schuylerwilliams <schuylerwilliams@Sky-Pro.local>
2016-12-11 11:13:05 -0600

    update

--- madison/js/initMap.js
+++ madison/js/initMap.js
@@ -75,13 +75,13 @@ map01.on('mousemove', function(e) {
     	var setHTML = "Stop Name: " + feature.properties.stop_name + "<br>" + "Stop Description: " + feature.properties.stop_desc + "<br>" + "Direction: " + feature.properties.direction +
     		"<br>" + "Route: " + feature.properties.Route;
     	popup.setLngLat(feature.geometry.coordinates)
-        	.setHTML(setHTML);
+        	.setHTML(setHTML)
         	.addTo(map01);
     } else if (feature.layer.id == 'bikeKohl1km' || feature.layer.id == 'bikeKohl2km' || feature.layer.id == 'bikeMonona1km' || feature.layer.id == 'bikeMonona2km' ||
     feature.layer.id == 'bikeYahara1km' || feature.layer.id == 'bikeYahara2km' || feature.layer.id == 'bikeMSN1km' || feature.layer.id == 'bikeMSN2km') {
     	var setHTML = "Name: " + feature.properties.Name + "<br>" + "Location: " + features.properties.Location;
     	popup.setLngLat(feature.geometry.coordinates)
-        	.setHTML(setHTML);
+        	.setHTML(setHTML)
         	.addTo(map01);
     }
 });
