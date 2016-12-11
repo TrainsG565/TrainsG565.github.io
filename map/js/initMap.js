191c97615bd5b513d2141ce89dd9b054c68c2ecd
schuylerwilliams <schuylerwilliams@Sky-Pro.local>
2016-12-11 13:10:31 -0600

    update

--- map/js/initMap.js
+++ map/js/initMap.js
@@ -4,7 +4,7 @@ mapboxgl.accessToken = 'pk.eyJ1Ijoic2t5d2lsbGlhbXMiLCJhIjoibUI4TlByNCJ9.9UuhBU3E
 // Initialize the map
 var map01 = new mapboxgl.Map({
     container: 'map01',
-    style: 'mapbox://styles/skywilliams/ciw5saqfs00042kpdz6yh0zpa',
+    style: 'mapbox://styles/skywilliams/ciwl0xx9000162pplymb6we97',
     center: [-89.4, 45],
     zoom: 5.75,
     pitch: 0.1,
