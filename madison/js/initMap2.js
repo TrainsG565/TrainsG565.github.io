9f5f364e1cb2e69bfc9f8dce906e9417c7c7f8b9
schuylerwilliams <schuylerwilliams@Sky-Pro.local>
2016-12-12 12:01:27 -0600

    update

--- madison/js/initMap2.js
+++ madison/js/initMap2.js
@@ -470,10 +470,10 @@ function removeBuffers2(x) {
 		if (position == 0) {
 			if (bufferPosition == 0) {
 				console.log('remove demo 1km kohl');
-				var spot = trackDemoBufferLayers2.indexOf(['==', 'GID', 1]);
-				trackDemoBufferLayers2.splice(spot, 1);
 				map02.setLayoutProperty('demo1km', 'visibility', 'none');
 				map02.setFilter('demo1km', ['==', 'GID', '']);
+				var spot = trackDemoBufferLayers2.indexOf(['==', 'GID', 1]);
+				trackDemoBufferLayers2.splice(spot, 1);
 			} else if (bufferPosition == 1) {
 				console.log('remove demo 2km kohl');
 				var spot = trackDemoBufferLayers2.indexOf(['==', 'GID', 5]);
