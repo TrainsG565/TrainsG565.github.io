59c474deaa10d630ebc5d9846e8eb2547b84f3d8
schuylerwilliams <schuylerwilliams@Sky-Pro.local>
2016-12-15 01:28:12 -0600

    update

--- madison/js/initMap2.js
+++ madison/js/initMap2.js
@@ -163,7 +163,18 @@ map02.on('click', function(e) {
     feature.layer.id == 'parcelYahara1km' || feature.layer.id == 'parcelYahara2km' || feature.layer.id == 'parcelMSN1km' || feature.layer.id == 'parcelMSN2km') {
     	
     	if (specialParcelControl == false) {
-			map02.setPitch(55);
+    		var myTimer = setInterval(myTimer, 50);
+			
+			var m = 0.1;
+			function myTimer() {
+    			m += 1;
+    			if (m >= 55) {
+    				clearInterval(myTimer);
+    			} else if (m < 55) {
+    				map02.setPitch(m);
+    			}
+			}
+			
     		var g; 
     		for (g=0; g < trackBufferLayers2.length; g++) {
     			if (trackBufferLayers2[g] == 'parcelKohl1km' || trackBufferLayers2[g] == 'parcelKohl1km' || trackBufferLayers2[g] == 'parcelMonona1km' || trackBufferLayers2[g] == 'parcelMonona2km' ||
